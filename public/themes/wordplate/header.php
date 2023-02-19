405cb079f775ea65b4ecd883b64cb80f09ff2961
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-02-19 17:36:40 +0100

    Change favicon
    Signed-off-by: Antoine Greuzard <antoine@antoinegreuzard.fr>

--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= get_home_url() ?>/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= get_home_url() ?>/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= get_home_url() ?>/favicon-16x16.png">
-    <link rel="manifest" href="<?= get_home_url() ?>/site.webmanifest">
-    <link rel="mask-icon" href="<?= get_home_url() ?>/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6da6c8">
+    <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
 
     <?php wp_head() ?>
