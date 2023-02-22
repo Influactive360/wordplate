21ea6003eed1d5032fa9b912805d6bfbc6e18eca
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-02-22 09:05:56 +0100

    FIX vite process
    Signed-off-by: Antoine Greuzard <antoine@antoinegreuzard.fr>

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,3 +4,7 @@ import "../css/styles.scss"
 import.meta.glob("../../themes/wordplate/templates/**/style.scss", {eager: true})
 // script
 import.meta.glob("../../themes/wordplate/templates/**/script.js", {eager: true})
+//images
+import.meta.glob('../static/img/**/*.{png,jpg,jpeg,gif,svg}', {eager: true})
+//fonts
+import.meta.glob('../static/fonts/**/*.{woff,woff2,ttf,eot}', {eager: true})
